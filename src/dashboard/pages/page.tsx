eb8ebb870037d19a1cfa66747802c405430dcc9e
Dimitris Dimopoulos <dimitrisdas2@gmail.com>
2024-06-19 03:33:28 +0300

    save latest

--- src/dashboard/pages/page.tsx
+++ src/dashboard/pages/page.tsx
@@ -1,60 +1,282 @@
+import React, { useState, ChangeEvent, useEffect } from 'react';
 import {
   Button,
-  EmptyState,
-  Image,
   Page,
-  TextButton,
   WixDesignSystemProvider,
-} from "@wix/design-system";
-import "@wix/design-system/styles.global.css";
-import * as Icons from "@wix/wix-ui-icons-common";
-import React from "react";
-import wixLogo from "./wix_logo.svg";
-import { withDashboard, useDashboard } from "@wix/dashboard-react";
+  Box,
+  Loader,
+  Input,
+  Heading
+} from '@wix/design-system';
+import '@wix/design-system/styles.global.css';
+import { useWixModules, WixProvider } from "@wix/sdk-react";
+import { embeddedScripts } from "@wix/app-market";
+import { dashboard, withDashboard } from '@wix/dashboard-react';
+import styles from './custom.module.css';
+import axios from 'axios';
 
-function Index() {
-  const { showToast } = useDashboard();
+interface Item {
+  id: string;
+  name: string;
+  imageUrl: string;
+  namespace: string;
+  hostName: string;
+}
+
+const loginToPlanpoint = async (email: string, password: string): Promise<string> => {
+  try {
+    const response = await axios.post('https://us-central1-planpoint-proxy.cloudfunctions.net/login', {
+      email,
+      password
+    });
+
+    const data = response.data;
+    return data._id;
+  } catch (err: any) {
+    if (err.response) {
+      alert(err.response.data.message || 'Login failed');
+    } else {
+      alert('Something went wrong. Please try again later');
+    }
+    return '';
+  }
+};
+
+const fetchItems = async (type: string, apiKey: string): Promise<Item[]> => {
+  const urlMap = {
+    projects: 'https://us-central1-planpoint-proxy.cloudfunctions.net/getProjects',
+    groups: 'https://us-central1-planpoint-proxy.cloudfunctions.net/getGroups',
+    enterprises: 'https://us-central1-planpoint-proxy.cloudfunctions.net/getEnterprises'
+  };
+
+  try {
+    // @ts-ignore
+    const response = await axios.get(urlMap[type], {
+      params: { uid: apiKey }
+    });
+
+    const data = response.data;
+  
+    return data.map((item: any) => ({
+      id: item._id,
+      name: item.name,
+      namespace: item.namespace,
+      hostName: item.hostName,
+      imageUrl: type === 'projects' ? item.projectImageUrl : type === 'groups' ? item.groupImageUrl : type === 'enterprises' ? item.enterpriseImageUrl : 'https://via.placeholder.com/50'
+    }));
+  } catch (error) {
+    // @ts-ignore
+    alert(`Failed to fetch items: ${error.message}`);
+    return [];
+  }
+};
+
+function Index() { 
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+  const [items, setItems] = useState<Item[]>([]);
+  const [selectedTab, setSelectedTab] = useState<string>('projects');
+  const [selectedItem, setSelectedItem] = useState<Item | null>(null);
+  const [loading, setLoading] = useState<string>('');
+  const [apiKey, setApiKey] = useState<string>('');
+  const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
+
+  const { embedScript } = useWixModules(embeddedScripts);
+
+  const handleLogin = async () => {
+    if (!email) {
+      alert('Enter your email please');
+      return;
+    }
+
+    if (!password) {
+      alert('Enter your password please');
+      return;
+    }
+
+    try {
+      setLoading('login');
+      const fetchedApiKey = await loginToPlanpoint(email, password);
+      setLoading('');
+
+      if (fetchedApiKey) {
+        setApiKey(fetchedApiKey);
+        setIsLoggedIn(true);
+        setSelectedTab('projects');
+        fetchItems('projects', fetchedApiKey);
+      }
+    } catch (err) {
+      setLoading('');
+    }
+  };
+
+  const handleTabChange = async (value: string) => {
+    setSelectedTab(value);
+    setItems([]);
+    setSelectedItem(null);
+    setLoading('items');
+    const fetchedItems = await fetchItems(value, apiKey);
+    setItems(fetchedItems);
+    setLoading('');
+  };
+
+  const handleEmailChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePasswordChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.target.value);
+  };
+
+  async function getAuthToken(clientId: string, clientSecret: string) {
+    const tokenEndpoint = 'https://www.wix.com/oauth/access';
+    const tokenParams = new URLSearchParams({
+      grant_type: 'client_credentials',
+      client_id: clientId,
+      client_secret: clientSecret,
+    });
+  
+    try {
+      const response = await axios.post(tokenEndpoint, tokenParams, {
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+      });
+  
+      return response.data.access_token;
+    } catch (error) {
+      console.error('Failed to get auth token:', error);
+      throw error;
+    }
+  }
+
+  const handleEmbedScript = async (type: string, item: Item) => {
+    try {
+      // Obtain the auth token
+      const clientId = "59303459-55ba-4c98-87ce-56ed2ad3d80e";
+      const clientSecret = "2ac417a1-1b5b-4344-bc0c-8371bf4e0588";
+      const authToken = await getAuthToken(clientId, clientSecret);
+
+      // Fetch existing embedded script parameters
+      const response = await axios.get('https://www.wixapis.com/apps/v1/scripts', {
+        headers: {
+          'Authorization': `Bearer ${authToken}`
+        }
+      });
+
+      console.log(response.data);
+
+      const existingParameters = response.data.properties.parameters;
+
+      // Update the parameters with the new project URL
+      const embedUrl = type === 'projects' ? 
+        `https://app.planpoint.io/${item.namespace}/${item.hostName}` : 
+        type === 'groups' ? 
+          `https://app.planpoint.io/g/${item.namespace}/${item.hostName}` : 
+          `https://app.planpoint.io/enterprise/${item.namespace}/${item.hostName}`;
+
+      const newParameters = {
+        ...existingParameters,
+        "projectUrl": embedUrl
+      };
+
+      await embedScript({
+        parameters: newParameters,
+      });
+
+      alert('Script embedded successfully!');
+    } catch (error: any) {
+      console.error('Failed to embed script:', error);
+      alert('Failed to embed script.');
+    }
+  }; 
+
+  useEffect(() => {
+    if (isLoggedIn) {
+      handleTabChange(selectedTab);
+    }
+  }, [isLoggedIn, selectedTab]);
 
   return (
-    <WixDesignSystemProvider>
-      <Page>
-        <Page.Header
-          title="Dashboard Page"
-          subtitle="Add management capabilities to your app."
-          actionsBar={
-            <Button
-              onClick={() => {
-                showToast({
-                  message: "Your first toast message!",
-                });
-              }}
-              prefixIcon={<Icons.GetStarted />}
-            >
-              Show a toast
-            </Button>
-          }
-        />
-        <Page.Content>
-          <EmptyState
-            image={
-              <Image fit="contain" height="100px" src={wixLogo} transparent />
-            }
-            title="Start editing this dashboard page"
-            subtitle="Learn how to work with dashboard pages and how to add functionality to them using Wix APIs."
-            theme="page"
-          >
-            <a
-              href="https://dev.wix.com/api/cli/app-framework/dashboard-pages"
-              target="_blank"
-            >
-              <TextButton prefixIcon={<Icons.ExternalLink />}>
-                Dashboard pages documentation
-              </TextButton>
-            </a>
-          </EmptyState>
-        </Page.Content>
-      </Page>
-    </WixDesignSystemProvider>
+    <WixProvider auth={dashboard.auth()} host={dashboard.host()}>
+      <WixDesignSystemProvider>
+        <Page>
+          <Page.Header
+            title="Embed your Planpoint"
+            subtitle="Select & automatically embed your Planpoint project, group or enterprise."
+          />
+          <Page.Content>
+          {!isLoggedIn ? (
+              <div>
+                <Heading appearance="H3">Login to your account</Heading>
+                <div style={{ marginTop: '20px' }}>
+                  <Input
+                    placeholder="Enter your email"
+                    value={email}
+                    onChange={handleEmailChange}
+                  />
+                </div>
+                <div style={{ marginTop: '16px', marginBottom: '16px' }}>
+                  <Input
+                    placeholder="Enter your password"
+                    value={password}
+                    type="password"
+                    onChange={handlePasswordChange}
+                  />
+                </div>
+                <Button onClick={handleLogin} disabled={loading === 'login'}>
+                  {loading === 'login' ? <Loader size="small" color="white" /> : 'Login'}
+                </Button>
+              </div>
+            ) : (
+              <>
+                {selectedItem ? (
+                  <div style={{ display: 'flex', alignItems: 'center', marginBottom: '20px' }}>
+                    <Button onClick={() => {
+                        setSelectedItem(null)
+                        const embedContainer = document.getElementById('embedContainer');
+                        // @ts-ignore
+                        embedContainer.innerHTML = ""
+                      }} style={{ marginRight: '10px' }}>Back</Button>
+                    <Heading appearance="H4">{selectedItem.name}</Heading>
+                  </div>
+                ) : (
+                  <div className={styles.tabs}>
+                    <button className={selectedTab === 'projects' ? styles.active : ''} onClick={() => handleTabChange('projects')}>Projects</button>
+                    <button className={selectedTab === 'groups' ? styles.active : ''} onClick={() => handleTabChange('groups')}>Groups</button>
+                    <button className={selectedTab === 'enterprises' ? styles.active : ''} onClick={() => handleTabChange('enterprises')}>Enterprises</button>
+                  </div>
+                )}
+                {!selectedItem && <Box className={styles.itemsContainer}>
+                  {items.map((item) => (
+                    <div
+                      key={item.id}
+                      onClick={() => {
+                        setSelectedItem(item)
+                        handleEmbedScript(selectedTab, item)
+                      }}
+                      style={{
+                        // @ts-ignore
+                        border: item.id === selectedItem?.id ? '2px solid blue' : '1px solid gray',
+                        padding: '10px',
+                        margin: '10px',
+                        display: 'flex',
+                        flexDirection: 'row',
+                        cursor: 'pointer',
+                      }}
+                    >
+                      <img src={item.imageUrl} alt={item.name} style={{ width: '50px', marginRight: '10px', height: '50px' }} />
+                      <span>{item.name}</span>
+                    </div>
+                  ))}
+                </Box>}
+                <div id="embedContainer"></div>
+              </>
+            )}
+          </Page.Content>
+        </Page>
+      </WixDesignSystemProvider>
+    </WixProvider>
   );
 }
 
